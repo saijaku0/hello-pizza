f5a2ad4e8218cd05bbfb321e95f3c4488411b55a
Vadym <minerking112256@gmail.com>
2024-11-09 17:03:31 +0200

    Add none visibility when invalid search input

--- components/shared/searchInput.tsx
+++ components/shared/searchInput.tsx
@@ -67,28 +67,31 @@ export const SearchInput = ({ className }: Props) => {
             setSearchQuery(e.target.value);
           }}
         />
-        <div
-          className={cn(
-            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
-            focused && "visible opacity-100 top-12",
-          )}
-        >
-          {products.map((product) => (
-            <Link
-              onClick={onClickItem}
-              key={product.id}
-              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-              href={`/product/${product.id}`}
-            >
-              <img
-                className="rounded-sm h-8 w-8"
-                src={product.imageUrl}
-                alt={product.name}
-              />
-              <span>{product.name}</span>
-            </Link>
-          ))}
-        </div>
+        
+        {products.length > 0 && (
+          <div
+            className={cn(
+              "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+              focused && "visible opacity-100 top-12",
+            )}
+          >
+            {products.map((product) => (
+              <Link
+                onClick={onClickItem}
+                key={product.id}
+                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                href={`/product/${product.id}`}
+              >
+                <img
+                  className="rounded-sm h-8 w-8"
+                  src={product.imageUrl}
+                  alt={product.name}
+                />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
