b27350a4646a1752aedf94e6e88121de61e36f04
Vadym <minerking112256@gmail.com>
2024-11-09 16:45:53 +0200

    Add search to header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 
 import { Container } from "./container";
 import { Button } from "../ui";
+import Link from "next/link";
+import { SearchInput } from "./searchInput";
 
 interface IProps {
   className?: string;
@@ -15,12 +17,18 @@ export const Header: React.FC<IProps> = ({ className }) => {
   return (
     <header className={cn("border border-p", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">Better taste</p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">Better taste</p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
 
         <div className="flex items-center gap-3">
